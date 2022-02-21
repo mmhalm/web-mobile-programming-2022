4dabca7cbc7ce46e58c198017a87950b1a52d5e1
mmhalm <mmhalm@utu.fi>
2022-02-21 17:41:11 +0200

    part2 somewhat done

--- courses/osa1-1/src/index.js
+++ courses/osa1-1/src/index.js
@@ -1,6 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>This is {props.course} </h1>
+    </div>
+  )
+}
+
+const Contents = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>Total {props.exercises1 + props.exercises2 + props.exercises3} exercises</p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Superadvanced web and mobile programming'
   const part1 = 'Basics of React'
@@ -12,11 +46,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-      <p>Total {exercises1 + exercises2 + exercises3} exercises</p>
+      <Header course={course} />
+      <Contents part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>      
     </div>
   )
 }
