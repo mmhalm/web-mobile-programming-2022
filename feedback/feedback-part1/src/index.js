a75627c24529bd208937b9298121844942992691
mmhalm <mmhalm@utu.fi>
2022-02-24 17:16:59 +0200

    first version of 1.6 done

--- feedback/feedback-part1/src/index.js
+++ feedback/feedback-part1/src/index.js
@@ -1,33 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      counter: 1
+    this.state1 = {
+      counter: 0
+    }
+    this.state2 = {
+      counter: 0
+    }
+    this.state3 = {
+      counter: 0
     }
   }
 
-  kasvataYhdella = () => {
-    this.setState({ counter: this.state.counter + 1 })
-  }
+  // kasvataYhdella = (props) => {
+  //   this.setState({ counter: this.state.counter + 1 })
+  // }
 
   render() {
+    const setToValue = (newValue) => () => { 
+      this.setState({ value: newValue })
+  }
+
     return (
       <div>
         <h1>anna palautetta</h1>
-        <div>{this.state.counter}</div>
         <div>
-          <button onClick={this.kasvataYhdella}>
-            plus
-          </button>
+          <button onClick={setToValue(this.state1.value+1)}>hyvä</button>
+          <button onClick={setToValue(this.state2.value+1)}>keskiverto</button>
+          <button onClick={setToValue(this.state3.value+1)}>huono</button>
         </div>
-        <h1>statistiikka</h1>
+        <div>
+          <h1>statistiikka</h1>
+          <p>hyvä {this.state1.value}</p>
+          <p>keskiverto {this.state2.value}</p>
+          <p>huono {this.state3.value}</p>
+        </div>
       </div>
-    )
+      )
   }
 }
 
