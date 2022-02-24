720fea4860893538ee7e4dd89875d5e4f857efbb
mmhalm <mmhalm@utu.fi>
2022-02-24 17:21:46 +0200

    part 1.6 done

--- feedback/feedback-part1/src/index.js
+++ feedback/feedback-part1/src/index.js
@@ -5,42 +5,51 @@ import './index.css';
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state1 = {
-      counter: 0
-    }
-    this.state2 = {
-      counter: 0
-    }
-    this.state3 = {
-      counter: 0
+    this.state = {
+      good: 0,
+      middle: 0,
+      bad: 0
     }
   }
 
-  // kasvataYhdella = (props) => {
-  //   this.setState({ counter: this.state.counter + 1 })
-  // }
+  klikGood = () => {
+    this.setState({
+      good: this.state.good + 1
+    })
+  }
+
+  klikMiddle = () => {
+    this.setState({
+      middle: this.state.middle + 1
+    })
+  }
+  
+  klikBad = () => {
+    this.setState({
+      bad: this.state.bad + 1
+    })
+  }
 
   render() {
-    const setToValue = (newValue) => () => { 
-      this.setState({ value: newValue })
-  }
-
     return (
       <div>
-        <h1>anna palautetta</h1>
         <div>
-          <button onClick={setToValue(this.state1.value+1)}>hyvä</button>
-          <button onClick={setToValue(this.state2.value+1)}>keskiverto</button>
-          <button onClick={setToValue(this.state3.value+1)}>huono</button>
+          <h1>anna palautetta</h1>
+          
+          <button onClick={this.klikGood}>hyvä</button>
+          <button onClick={this.klikMiddle}>keskiverto</button>
+          <button onClick={this.klikBad}>huono</button>
+
+          {this.state.oikea}
         </div>
         <div>
           <h1>statistiikka</h1>
-          <p>hyvä {this.state1.value}</p>
-          <p>keskiverto {this.state2.value}</p>
-          <p>huono {this.state3.value}</p>
+          {this.state.good}
+          {this.state.middle}
+          {this.state.bad}
         </div>
       </div>
-      )
+    )
   }
 }
 
