9dce17c838e49f4a67d1cefd247e1060a42f80ac
mmhalm <mmhalm@utu.fi>
2022-03-01 11:27:11 +0200

    part 1.8 done

--- feedback/feedback-part1/src/index.js
+++ feedback/feedback-part1/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -14,28 +15,53 @@ class App extends React.Component {
     }
   }
 
-  klikGood = () => {
-    this.setState({
-      good: this.state.good + 1
-    })
-    this.updateStatistics()
-  }
+  // klikGood = () => {
+  //   this.setState({
+  //     good: this.state.good + 1
+  //   })
+  //   this.updateStatistics()
+  // }
 
-  klikMiddle = () => {
-    this.setState({
-      middle: this.state.middle + 1
-    })
-    this.updateStatistics()
+  // klikMiddle = () => {
+  //   this.setState({
+  //     middle: this.state.middle + 1
+  //   })
+  //   this.updateStatistics()
+  // }
+  
+  // klikBad = () => {
+  //   this.setState({
+  //     bad: this.state.bad + 1
+  //   })
+  //   this.updateStatistics()
+  // }
+
+  asetaArvoon1 = (arvo) => {
+    return() => {
+      this.setState({ good: arvo })
+      this.updateAverage()
+    }
+  }
+  asetaArvoon2 = (arvo) => {
+    return() => {
+      this.setState({ middle: arvo })
+      this.updateAverage()
+    }
+  }
+  asetaArvoon3 = (arvo) => {
+    return() => {
+      this.setState({ bad: arvo })
+      this.updateAverage()
+    }
   }
   
-  klikBad = () => {
-    this.setState({
-      bad: this.state.bad + 1
-    })
-    this.updateStatistics()
-  }
+  // asetaArvoon2 = (arvo) => () => this.setState({ middle: arvo })
+  // asetaArvoon3 = (arvo) => () => this.setState({ bad: arvo })
 
-  updateStatistics = () => {
+  asetaArvoon4 = (arvo) => () => this.setState({average: arvo})
+  asetaArvoon5 = (arvo) => () => this.setState({pos: arvo})
+
+  updateAverage = () => {
     this.setState({
       average: (this.state.bad*(-1) + this.state.good) / (this.state.bad + this.state.good + this.state.middle),
       pos: this.state.good / (this.state.bad + this.state.good + this.state.middle) * 100
@@ -48,23 +74,85 @@ class App extends React.Component {
         <div>
           <h1>anna palautetta</h1>
           
-          <button onClick={this.klikGood}>hyvä</button>
-          <button onClick={this.klikMiddle}>neutraali</button>
-          <button onClick={this.klikBad}>huono</button>
+          {/* <button onClick={this.klikGood}>hyvä</button> */}
+          <Button
+            handleClick={this.asetaArvoon1(this.state.good + 1)}
+            // handleClick={this.asetaArvoon4((this.state.bad*(-1) + this.state.good) / (this.state.bad + this.state.good + this.state.middle))
+            // }
+            text="hyvä"
+          />
+          <Button
+            handleClick={this.asetaArvoon2(this.state.middle + 1)}
+            text="neutraali"
+          />
+          <Button
+            handleClick={this.asetaArvoon3(this.state.bad + 1)}
+            text="huono"
+          />
+          {/* <button onClick={this.klikMiddle}>neutraali</button>
+          <button onClick={this.klikBad}>huono</button> */}
 
         </div>
         <div>
           <h1>statistiikka</h1>
-          <p>hyvä {this.state.good}</p>
-          <p>neutraali {this.state.middle}</p>
-          <p>huono {this.state.bad}</p>
-          <p>keskiarvo {this.state.average}</p>
-          <p>positiivista {this.state.pos} %</p>
+          {/* <Statistic 
+            handleClick={this.asetaArvoon4((this.state.bad*(-1) + this.state.good) / (this.state.bad + this.state.good + this.state.middle))}
+          />
+          <Statistic 
+          handleClick={this.asetaArvoon5(this.state.good / (this.state.bad + this.state.good + this.state.middle) * 100)}
+          /> */}
+          <Statistics info={this.state} />
         </div>
       </div>
     )
   }
 }
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) => (
+  <div>
+    <p>hyvä {props.info.good}</p>
+    <p>neutraali {props.info.middle}</p>
+    <p>huono {props.info.bad}</p>
+    <p>keskiarvo {props.info.average}</p>
+    <p>positiivisia {props.info.pos} %</p>
+  </div>
+)
+
+const Statistic = (props) => (
+  props.handleClick
+)
+
+// class Statistic extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       average: props.info.average, 
+//       positives: props.info.pos
+//     }
+//   }
+
+//   updateStatistics = () => {
+//     this.setState({
+//       average: (this.props.info.bad*(-1) + this.props.info.good) / (this.props.info.bad + this.props.info.good + this.props.info.middle),
+//       positives: this.props.info.good / (this.props.info.bad + this.props.info.good + this.props.info.middle) * 100
+//     })
+//   }
+
+//   render(){
+//     return(
+//       this.updateStatistics()
+//       [this.state.average,
+//       this.state.positives]
+//     )
+//   }
+// }
+
 
 ReactDOM.render(
   <App />,
